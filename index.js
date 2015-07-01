1f98aad57b9e3c99c16278ddf8296235b4d160e6
ruchiraw <ruchira.wageesha@gmail.com>
2015-07-01 16:22:43 +0530

    updating

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var cdn = 'https://d1vda6a1j3uyzl.cloudfront.net/';
 
-module.exports.cdn = function (items) {
+module.exports.cdn = function () {
+    return cdn;
+};
+
+module.exports.cdn288x162 = function (items) {
     if (!items) {
-        return cdn;
+        return cdn + 'images/288x162/';
     }
     var o = items instanceof Array ? items : [items];
     o.forEach(function (item) {
@@ -10,9 +14,36 @@ module.exports.cdn = function (items) {
         if (!photos) {
             return;
         }
+        var o = [];
         photos.forEach(function (photo, i) {
-            photos[i] = cdn + photo;
+            o.push({
+                id: photo,
+                url: cdn + 'images/288x162/' + photo
+            });
         });
+        item.photos = o;
+    });
+    return items;
+};
+
+module.exports.cdn800x450 = function (items) {
+    if (!items) {
+        return cdn + 'images/800x450/';
+    }
+    var o = items instanceof Array ? items : [items];
+    o.forEach(function (item) {
+        var photos = item.photos;
+        if (!photos) {
+            return;
+        }
+        var o = [];
+        photos.forEach(function (photo, i) {
+            o.push({
+                id: photo,
+                url: cdn + 'images/800x450/' + photo
+            });
+        });
+        item.photos = o;
     });
     return items;
 };
